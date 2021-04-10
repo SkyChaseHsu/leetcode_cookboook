06d3682c732a951c868a06429abf1e3e396a8026
skyChaseHsu <skychase@163.com>
2021-04-10 23:02:54 +0800

    整理文件

--- get_links.py
+++
@@ -1,7 +0,0 @@
-from lxml.html import etree
-
-with open("./leetcode_all.html", "r", encoding='utf-8') as f:
-	html_content = etree.HTML("".join(f.readlines()))
-	target = html_content.xpath("//div[@class='question-title']/a/@href")
-	print(target)
-
